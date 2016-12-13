e5d0b79a0f927665623b770587929107e6eedd73
supreeth-sindhey <supreeth_shindhey@yahoo.com>
2016-12-14 00:47:49 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GETnPOST.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Get N Post</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <br /><br />
+        Type of http context / Type of request from Client : <asp:Label Text="" id="lblRequestType" runat="server" />
+        <br /><br />
+        <asp:Button Text="Click here" runat="server" ID="btnClickHere" OnClick="btnClickHere_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GETnPOST
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string requestType = HttpContext.Current.Request.HttpMethod;
+
+            lblRequestType.Text = requestType + "--> On page load";
+           
+        }
+
+        protected void btnClickHere_Click(object sender, EventArgs e)
+        {
+
+            string requestType = HttpContext.Current.Request.HttpMethod;
+
+            lblRequestType.Text = requestType + " --> When button is clicked / or an event is triggered within the page";
+        }
+    }
+}
